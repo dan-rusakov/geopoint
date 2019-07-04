d31e376a97ba3101aaec87b21caa8d32c221bb19
dan3439 <39083213+dan3439@users.noreply.github.com>
2019-07-04 09:39:42 +0500

    final update

--- app/js/common.js
+++ app/js/common.js
@@ -7,4 +7,9 @@ $(function() {
 
 	});
 
+	$('.open-popup').magnificPopup({
+		type:'inline',
+		midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+	});
+
 });
