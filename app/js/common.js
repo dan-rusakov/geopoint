ed01b303c845ae25f1d301dcda2ec019063b6c6e
dan3439 <39083213+dan3439@users.noreply.github.com>
2019-07-31 17:42:57 +0500

    add carousel

--- app/js/common.js
+++ app/js/common.js
@@ -7,6 +7,12 @@ $(function() {
 
 	});
 
+	$('.partners-carousel').owlCarousel({
+		items: 3,
+		dots: true,
+		nav: false
+	})
+
 	$('.open-popup').magnificPopup({
 		type:'inline',
 		midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
